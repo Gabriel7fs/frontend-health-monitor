298871780c8bdf073bd1cef44bfded1b78ebf94e
Gabriel Furquim da Silva <88550409+Gabriel7fs@users.noreply.github.com>
2024-09-15 01:21:38 -0300

    feature-login-page - tela de login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,21 +3,25 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { StartScreenComponent } from './start-screen.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaskitoDirective  } from '@maskito/angular';
+import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
+    MaskitoDirective,
     RouterModule.forChild([
       {
         path: '',
-        component: StartScreenComponent
+        component: LoginComponent
       }
     ])
   ],
-  declarations: [StartScreenComponent],
-  exports: [StartScreenComponent]
+  declarations: [LoginComponent],
+  exports: [LoginComponent]
 })
-export class StartScreenModule {}
+export class LoginModule {}
---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-start-screen',
+  templateUrl: './start-screen.component.html',
+  styleUrls: ['./start-screen.component.scss'],
+})
+export class StartScreenComponent {
+
+  public logoApp = '../../assets/img/logo.png';
+
+  constructor(
+    private router: Router
+  ) { }
+
+  onClickLogin(): void {
+    this.router.navigateByUrl('/login');
+  }
+
+  onClickRegister(): void {
+    alert("Em desenvolvimento");
+  }
+
+}
